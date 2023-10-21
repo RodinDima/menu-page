74d7e7fd67c5c474b67e82001392e0fced3a72c9
akilianchuiko <akilianchuiko@gmail.com>
2023-10-21 12:14:43 +0300

    add likes

--- src/Components/SaladsCard/SaladsCard.jsx
+++ src/Components/SaladsCard/SaladsCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import { ReactComponent as Like } from "./Image/like.svg";
 
@@ -7,18 +7,35 @@ import { CardLeft, CardRight } from "../../ConstantsCard";
 import "./style.css";
 
 const SaladsCard = () => {
+
+  const [like, setLike] = useState(false);
+
+  const [listLikeIds, setListLikeIds] = useState([])
+
+
+
+  const handleToogleLike = useCallback((likeid) => {
+    if (listLikeIds.includes(likeid)) {
+      setListLikeIds(listLikeIds.filter((id) => id !== likeid));
+      setLike(!like);
+    } else {
+      setListLikeIds([...listLikeIds, likeid]);
+    }
+  }, [listLikeIds, like]);
+
+
   return (
     <>
       <div className="salads__inner">
         <div className="salads__card-left">
           {CardLeft.map((item) => (
-            <a href="#" className="salads__card">
-              <div className="salads__card-item" key={item.id}>
+            <a href="#" className="salads__card" key={item.id}>
+              <div className="salads__card-item">
                 <div className="salads__card-img">
                   <img src={item.photo} alt="#" />
                 </div>
-
-                <Like className="like" />
+                <Like onClick={() => handleToogleLike(item.id)} className={`like ${listLikeIds.includes(item.id) ? 'active' : ''}`} />
+                
 
                 <p className="salads__card-title">{item.name}</p>
                 <p className="salads__card-descr">{item.descr}</p>
